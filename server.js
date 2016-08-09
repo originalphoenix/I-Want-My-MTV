380626cace2366904b1dc4ee5217b66db40b7926
Samantha <samantha@quantumaspects.com>
2016-08-09 10:10:47 -0700

    accidentally deleted a comment and deleting some more scripts and stuff

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 // set static files location
 app.use(express.static(__dirname + '/public'));
 
-/ set up home page
+// set up home page
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
