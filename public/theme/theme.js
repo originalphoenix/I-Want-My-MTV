7cf1cccbf1e55b2b019218056cc22a4892fcbc52
Samantha <samantha@quantumaspects.com>
2016-08-08 18:19:23 -0700

    started setting up routes and views

---
+++ public/theme/theme.js
@@ -0,0 +1,35 @@
+    var themeApp = angular.module('themeApp', ['ngRoute']);
+
+    // configure our routes
+    themeApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the profile page
+            .when('/profile', {
+                templateUrl : 'pages/profile.html',
+                controller  : 'profileController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/genres.html',
+                controller  : 'genreController'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    themeApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+    });
+
+    themeApp.controller('profileController', function($scope) {
+    });
+
+    themeApp.controller('genreController', function($scope) {
+    });
